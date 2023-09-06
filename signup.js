56fd8210d38f6a2194f43560d366cb2e7940e06f
Nouman <noumanafzal.business@gmail.com>
2023-09-06 21:17:23 +0500

    validation

--- signup.js
+++ signup.js
@@ -1,39 +1,98 @@
-// Event Listener of Register Button
-const signupForm = document.getElementById('btn');
-signupForm.addEventListener('click' , (e) => {
-    e.preventDefault();
-    checkLength(ePass, 3, 8)
-})
-
-// For Error Msgs
-let inputBox = document.getElementsByClassName('input-box');
+let signupForm = document.getElementById('signupForm');
+let uName = document.getElementById('uName');
+let email = document.getElementById('email');
+let ePass = document.getElementById('ePass');
+let cPass = document.getElementById('cPass');
+let inputBox = document.getElementById('inputBox')
 
 
 
-// SignUp form validation
-const uName = document.getElementById('uName');
-const email = document.getElementById('email');
-const ePass = document.getElementById('ePass');
-const cPass = document.getElementById('cPass');
 
 
-
-const checkLength = (field,min,max) => {
-    const text = field.value.trim();
-    if(text.length < min) {
-        if(!field.parentNode.classList.contains('error'))
-            field.parentNode.classList.add('error')
-            return;
-    }
-    if(text.length > max) {
-        if(!field.parentNode.classList.contains('error'))
-            field.parentNode.classList.add('error')
-            return;
-    }
-    if(field.parentNode.classList.contains('error'))
-    field.parentNode.classList.remove('error')
-    if(!field.parentNode.classList.contains('success'))
-    field.parentNode.classList.add('success')
+// Name Feild Length validation
+const checkLengthName = (field, min, max) => {
+    let text = field.value.trim();   //for removing extra spaces
+    if( text === '')
+        {
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }  
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+         else if(text < min){
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+         else if(text > max){
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+        else{
+            if(inputBox.classList.contains('error'))
+                {
+                    inputBox.classList.remove('error');
+                }
+                inputBox.classList.add('success')
+            }
 
 }
 
+
+const checkLengthEmail = (field, min, max) => {
+    let text = field.value.trim();   //for removing extra spaces
+    if( text === '')
+        {
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }  
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+         else if(text < min){
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+         else if(text > max){
+            if(inputBox.classList.contains('success'))
+                {
+                    inputBox.classList.remove('success');
+                }
+                inputBox.classList.add('error');
+                errorMsg.innerText = "please enter name";
+        }
+        else{
+            if(inputBox.classList.contains('error'))
+                {
+                    inputBox.classList.remove('error');
+                }
+                inputBox.classList.add('success')
+        } }
+
+
+
+
+
+//event listener for register button
+const regBtn = document.getElementById('btn');
+regBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    checkLengthName(uName,4,12);
+    checkLengthEmail(email,4,15);
+    
+})
